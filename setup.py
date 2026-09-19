3efaedb454278de68b8f4202696310e6801de005
White-Codes <110305028+White-Codes@users.noreply.github.com>
2026-09-19 11:33:05 +0100

    Add install_requires and dependency_links to setup.py
    Added required dependencies for the project.

--- setup.py
+++ setup.py
@@ -5,4 +5,19 @@ setup(
     version="0.1.0",
     packages=find_packages(),
     python_requires=">=3.10",
+    install_requires=[
+        "stable-baselines3==2.3.2",
+        "gymnasium==0.29.1",
+        "torch==2.2.0",
+        "pandas==2.2.0",
+        "numpy==1.26.4",
+        "yfinance==0.2.36",
+        "pyyaml==6.0.1",
+        "loguru==0.7.2",
+        "matplotlib==3.8.2",
+        "tqdm==4.66.1",
+    ],
+    dependency_links=[
+        "git+https://github.com/twopirllc/pandas-ta.git@development#egg=pandas-ta"
+    ]
 )
